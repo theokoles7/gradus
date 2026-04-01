aa6a65d33a5456f113552d7ba405cb17a693eb6c
theokoles7 <gabrieltrahan777@hotmail.com>
2026-04-01 10:49:08 -0500

    Update experiments

--- experiments/baseline.sh
+++ experiments/baseline.sh
@@ -4,7 +4,7 @@ for model in resnet-18 resnet-34 resnet-50 resnet-101 resnet-152 vgg-16 vgg-19; 
 
     for dataset in mnist cifar-10 cifar-100; do
 
-        gradus train $model $dataset
+        gradus train --epochs 200 $model $dataset --batch-size 128
 
     done
 
