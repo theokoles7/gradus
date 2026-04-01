6e7529c257c7feb5b2707ace65352d71568cb00a
theokoles7 <gabrieltrahan777@hotmail.com>
2026-04-01 08:47:55 -0500

    Initialize experiment scripts

---
+++ experiments/random_shuffle.sh
@@ -0,0 +1,11 @@
+# Random Shuffle Runs
+
+for model in resnet-18 resnet-34 resnet-50 resnet-101 resnet-152 vgg-16 vgg-19; do
+
+    for dataset in mnist cifar-10 cifar-100; do
+
+        gradus train $model $dataset --shuffle
+
+    done
+
+done
